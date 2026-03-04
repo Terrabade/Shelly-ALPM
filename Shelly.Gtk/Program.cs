993c028a87290623735db4e64e96697d822c5053
Zoey <zoey.erin.bauer@gmail.com>
2026-03-04 12:59:24 -0500

    Initialize GTK application with basic UI structure
    - Added `MainWindow`, `MainMenu`, and `HomeWindow` UI files for the application layout.
    - Updated `Shelly.Gtk.csproj` to include `.ui` files with `CopyToOutputDirectory`.
    - Implemented `Program.cs` with GTK application setup, menu actions, and content area management.

--- Shelly.Gtk/Program.cs
+++ Shelly.Gtk/Program.cs
@@ -1,3 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Gtk;
+
+var application = Gtk.Application.New("com.zcdevelopment.shelly", Gio.ApplicationFlags.DefaultFlags);
+
+application.OnActivate += (sender, args) =>
+{
+    var mainBuilder = Builder.NewFromFile("UiFiles/MainWindow.ui");
+    var window = (ApplicationWindow)mainBuilder.GetObject("MainWindow")!;
+    window.Application = application;
+
+    var menuBuilder = Builder.NewFromFile("UiFiles/MainMenu.ui");
+    var appMenu = (Gio.Menu)menuBuilder.GetObject("AppMenu")!;
+    application.Menubar = appMenu;
+
+    var quitAction = Gio.SimpleAction.New("quit", null);
+    quitAction.OnActivate += (sender, args) => application.Quit();
+    application.AddAction(quitAction);
+
+    var preferencesAction = Gio.SimpleAction.New("preferences", null);
+    preferencesAction.OnActivate += (sender, args) => Console.WriteLine("Preferences clicked");
+    application.AddAction(preferencesAction);
+
+    var aboutAction = Gio.SimpleAction.New("about", null);
+    aboutAction.OnActivate += (sender, args) => Console.WriteLine("About clicked");
+    application.AddAction(aboutAction);
+
+    var contentArea = (Box)mainBuilder.GetObject("ContentArea")!;
+
+    var homeBuilder = Builder.NewFromFile("UiFiles/HomeWindow.ui");
+    var homeBox = (Box)homeBuilder.GetObject("HomeWindow")!;
+
+    contentArea.Append(homeBox);
+
+    window.Show();
+};
+
+return application.Run(args);
